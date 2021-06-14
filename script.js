7b667d9e881bf0fc867eb49b59a0c3a092c20c2e
LeoKirasic <kirasicleo@gmail.com>
2021-06-14 20:46:12 +0200

    Rewrote the javascript code made grid proportional

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const container = document.querySelector('#container');
-let root = document.querySelector(':root');
+const root = document.querySelector(':root');
 let grid;
-function createGrid(divNumber) {
 
-    for(let i = 0; i < divNumber; i++) {
-        for(let j = 0; j < divNumber; j++) {
-            grid = document.createElement('div');
-            grid.style.borderStyle = 'solid';
-            grid.style.borderWidth = '1px';
-            grid.addEventListener('mouseover', function (e) {
-                e.target.style.backgroundColor = 'black';
-            })
-            container.appendChild(grid);
-        } 
-    }
+let input = prompt('enter the grid size');
+root.style.setProperty('--grid-rows', input);
+root.style.setProperty('--grid-columns', input);
+
+if(input < 1 || input > 64) {
+    alert('invalid input!');
+    window.location.reload(true);
 }
+createGrid(input);
 
-const clearGrid = document.querySelector('#clear-grid');
+const clearGrid = document.querySelector('#clear-button');
 
 clearGrid.addEventListener('click', () => {
-    let input = prompt('enter the grid size');
-    root.style.setProperty('--grid-rows', input);
-    root.style.setProperty('--grid-columns', input);
-    createGrid(input);
+    window.location.reload(true);
 });
+
+function colorOnHover (e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+function createGrid (number) {
+    for(let i = 0; i < number; i++) {
+        for(let j = 0; j < number; j++) {
+            grid = document.createElement('div');
+            grid.classList.add('grid')
+            grid.style.borderStyle = 'solid';
+            grid.style.borderWidth = '1px';
+            grid.addEventListener('mouseover', colorOnHover);
+            container.appendChild(grid);
+        }
+    }
+}
